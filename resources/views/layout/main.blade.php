1702483ba617a139da9e8e64cd63a5f522859789
aidensV <nameblacllis@gmail.com>
2021-06-14 19:54:34 +0700

    delete route

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -39,7 +39,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     <!-- Left navbar links -->
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a href="index3.html" class="nav-link">Home</a>
+                            <a href="#" class="nav-link">Home</a>
                         </li>
                         
                       
