9bfdd93da5d7ef4eb1324ce0ad0a62a3e98f7341
aidensV <nameblacllis@gmail.com>
2021-06-14 18:24:34 +0700

    add schema

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class HistoryController extends Controller
 {
+
+    public function index()
+    {
+        
+    }
     public function store(Request $request)
     {
         try {
