adbd63b57456c8de20e31ca0fc7a35fd66ba415a
aidensV <nameblacllis@gmail.com>
2021-06-16 20:12:49 +0700

    full path image

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -9,9 +9,24 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Module extends Model
 {
     use HasFactory,SoftDeletes;
-
+    protected $appends = ['full_path_image','full_path_file'];
     public function lab()
     {
         return $this->belongsTo(Lab::class,'lab_id','id');
+    }
+
+    public function getFullPathImageAttribute()
+    {
+        if($this->attributes['path_image']){
+            return asset('storage/berkas/'.$this->attributes['path_image']);    
+        }
+        return '-';
+    }
+    public function getFullPathFileAttribute()
+    {
+        if($this->attributes['path_file']){
+            return asset('storage/berkas/dokumen/'.$this->attributes['path_file']);    
+        }
+        return '-';
     }
 }
